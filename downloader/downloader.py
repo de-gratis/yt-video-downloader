8fb4e1142ba95c7479747b470874fe43296bf9c9
Ninniet5670 <marconeto3000@gmail.com>
2023-07-29 03:15:59 -0300

    mp3 and progress bar added

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,31 +1,35 @@
+import os
 from pytube import YouTube, Playlist
 from pytube.cli import on_progress
 
 
-def baixar_yt_video(link):
+def baixar_yt_video(link:str, mp3:bool=False) -> None:
     while True:
         if link == 'n':
             break
         else:
             try:
-                yt = YouTube(link,on_progress_callback=on_progress)
+                yt = YouTube(link, on_progress_callback=on_progress)
 
                 print(f'O título deste vídeo é: {yt.title}')
-            except Exception as error:
-                print('ERRO: ', error)
-                continue
 
-            try:
-                yt.streams.get_highest_resolution().download()
+                if mp3:
+                    out_file = yt.streams.filter(only_audio=True).first().download(output_path='downloads')
+                    base = os.path.splitext(out_file)[0]
+                    new_file = base + '.mp3'
+                    os.rename(out_file, new_file)
+                else:
+                    yt.streams.get_highest_resolution().download(output_path='downloads')
                 print('Download concluído com sucesso')
                 break
             except Exception as error:
                 print('ERRO: ', error)
+                continue
 
 
-def baixar_yt_playlist(link):
+def baixar_yt_playlist(link:str, mp3:bool) -> None:
     playlist = Playlist(link)
     print(f'Número de vídeo na playlist: {len(playlist.video_urls)}')
 
     for video_url in playlist.video_urls:
-        baixar_yt_video(video_url)
+        baixar_yt_video(video_url, mp3)
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from downloader import baixar_yt_video, baixar_yt_playlist
 
 menu = '''
-[1] Baixe um vídeo do YouTube colando seu link
-[2] Baixe uma playlist do YouTube colando seu link
+### Em .mp4
+[1] Baixar vídeo
+[2] Baixar playlist
+### Em .mp3
+[3] Baixar vídeo
+[4] Baixar playlist
 Digite sua opção: '''
 
 if __name__ == '__main__':
     while True:
-        escolha = input(menu)
-        if escolha == '1':
-            baixar_yt_video(input('Digite o link do seu vídeo: '))
-        elif escolha == '2':
-            baixar_yt_playlist(input('Digite o link da sua playlist: '))
-        else:
-            break
+        match input(menu):
+            case '1':
+                baixar_yt_video(input('Digite o link do seu vídeo: '))
+            case '2':
+                baixar_yt_playlist(input('Digite o link da sua playlist: '))
+            case '3':
+                baixar_yt_video(input('Digite o link do seu vídeo: '), True)
+            case '4':
+                baixar_yt_playlist(input('Digite o link da sua playlist: '), True)
+            case _:
+                break
